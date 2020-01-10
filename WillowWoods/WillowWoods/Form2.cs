830d677b1ea5e2acbf9aaa154ec99388ee00000c
KWass1112 <57112616+KWass1112@users.noreply.github.com>
2020-01-10 14:12:24 -0600

    1/10/20

--- WillowWoods/WillowWoods/Form2.cs
+++ WillowWoods/WillowWoods/Form2.cs
@@ -17,6 +17,33 @@ namespace WillowWoods
             InitializeComponent();
         }
 
-       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int userEntry;
+            int choice;
+            Random ranNumberGenerator = new Random();
+            choice = ranNumberGenerator.Next(1, 2);
+
+            Console.WriteLine("Looks like you're almost close to the exit! Now pick a door but be warned a monster may be waiting behind one. Choose wisely");
+            userEntry = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                if (userEntry == choice)
+                {
+                    Console.WriteLine("Yay you chose the right door! You're now free!");
+                    Console.WriteLine("--------------------------------------------------------------------");
+                    DisplayGameEnd();
+                    // In form would change panel 
+                }
+                else
+                {
+                    MonsterPanel();
+
+                    // In form would change panel 
+                }
+
+            } while (userEntry != choice);
+        }
     }
 }
